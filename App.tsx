7e47d66cdc1e6f80908b0a3239ccb2cd3d573033
danawise <73659727+danawise@users.noreply.github.com>
2021-08-11 14:12:51 -0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,1 +1,33 @@
 
+* @format
+ */
+ import 'react-native-gesture-handler';
+ import React from 'react';
+ import {SafeAreaView, View, StatusBar, useColorScheme} from 'react-native';
+ import Home from './src/screens/PagerView/Home'
+ import {Colors} from 'react-native/Libraries/NewAppScreen';
+ import Router from './src/router';
+ import Amplify , {Auth} from 'aws-amplify'
+ import { withAuthenticator } from 'aws-amplify-react-native'
+import config from './src/aws-exports'
+Amplify.configure(config)
+ 
+ const App = () => {
+   const isDarkMode = useColorScheme() === 'dark';
+ 
+   const backgroundStyle = {
+     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+     flex: 1,
+   };
+   return (
+     <View style={backgroundStyle}>
+       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+       <Router />
+        {/* {<BlurTest/>>} */}
+        
+        
+     </View>
+   );
+ };
+ 
+ export default withAuthenticator(App);
